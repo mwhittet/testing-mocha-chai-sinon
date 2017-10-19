5d0aabf42491cca5efc047add43f6775b117ec2d
Michael Whittet <5453237+mwhittet@users.noreply.github.com>
2017-10-19 11:27:51 +0100

    Quick up to speed tutorial.

---
+++ cow.js
@@ -0,0 +1,17 @@
+// cow.js
+(function(exports) {
+  "use strict";
+
+  function Cow(name) {
+    this.name = name || "Anon cow";
+  }
+  exports.Cow = Cow;
+
+  Cow.prototype = {
+    greets: function(target) {
+      if (!target)
+        return console.error("missing target");
+      console.log(this.name + " greets " + target);
+    }
+  };
+})(this);
